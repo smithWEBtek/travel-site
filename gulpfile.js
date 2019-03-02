fbe773a40e1efe8773b521f24be562a8a6283722
smithwebtek <brad@smithwebtek.com>
2019-03-02 16:31:58 -0500

    Thru 6

--- gulpfile.js
+++ gulpfile.js
@@ -4,29 +4,40 @@ var gulp = require('gulp'),
   autoprefixer = require('autoprefixer'),
   cssvars = require('postcss-simple-vars'),
   nested = require('postcss-nested'),
-  cssImport = require('postcss-import');
+  cssImport = require('postcss-import'),
+  browserSync = require('browser-sync').create();
 
 gulp.task('default', function () {
   console.log('hurray you created a gulp task');
 })
 
 gulp.task('html', function () {
-  console.log('useful html thing...');
-})
+  console.log("Imagine something useful being done to your HTML here.");
+});
 
 gulp.task('styles', function () {
-  return gulp.src('./app/assets/styles/styles.scss')
+  return gulp.src('./app/assets/styles/styles.css')
     .pipe(postcss([cssImport, cssvars, nested, autoprefixer]))
     .pipe(gulp.dest('./app/temp/styles'));
-
 })
 
 gulp.task('watch', function () {
+  browserSync.init({
+    server: {
+      baseDir: 'app'
+    }
+  });
+
   watch('./app/index.html', function () {
-    gulp.start('html');
+    browserSync.reload();
   })
 
-  watch('./app/assets/styles/**/*.scss', function () {
-    gulp.start('styles');
+  watch('./app/assets/styles/**/*.css', function () {
+    gulp.start('cssInject');
   })
 })
+
+gulp.task('cssInject', ['styles'], function () {
+  return gulp.src('./app/temp/styles/styles.css')
+    .pipe(browserSync.stream());
+});
